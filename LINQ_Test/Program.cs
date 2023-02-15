c766ce648ffa4b9de795f2ce8721767c1b85e22a
pranit patil <pranitpc48@gmail.com>
2023-02-15 11:12:56 +0530

    austraila added

--- LINQ_Test/Program.cs
+++ LINQ_Test/Program.cs
@@ -10,7 +10,7 @@ public class Program
         {
 
                 "balochistan",
-
+                "austrailia",
                 "Cabo Verde",
                 "Cambodia",
                 "Cameroon",
